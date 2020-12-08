8529fd67c9383f67272077652d9b21181fd8c07e
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 15:14:03 -0300

    Demo of Block finally

---
+++ Block-finally/Block-finally/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+public class ProcessFile
+{
+    public static void Main()
+    {
+        FileStream fs = null;
+        try
+        {
+            fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        //The finally block is a block that contains a code to be executed regardless of whether or not an exception has occurred. 
+        finally
+        {
+            if (fs != null)
+            {
+                fs.Close();
+            }
+        }
+    }
+}
