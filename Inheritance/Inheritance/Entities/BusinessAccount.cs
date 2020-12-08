3015d9811c531ecfc1ae89b8b247beb765e515d8
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-07 22:11:20 -0300

    Demo of Inheritance: Inheritance example and using the access modifier 'protected'

---
+++ Inheritance/Inheritance/Entities/BusinessAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace Inheritance.Entities
+{
+    class BusinessAccount : Account  // BusinessAccount inherits from Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+        public void loanLimit(double amount)
+        {
+            if (amount < LoanLimit)
+                Balance += amount;
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using Inheritance.Entities;
+using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Suppose a bank business that has a common account and a business account, and the business account has all members of the common account,
+             *plus a loan limit and a loan operation.*/
+
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0; // error
+        }
+    }
+}
