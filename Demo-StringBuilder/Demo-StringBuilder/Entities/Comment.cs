d6f9a698527aaee8ceb9c4c14ef83e9092bc17cc
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-07 21:05:18 -0300

    Demo of StringBuilder: Commenting in a post

---
+++ Demo-StringBuilder/Demo-StringBuilder/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo_StringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
