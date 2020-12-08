2527fb7449363fbe23ed5b2c9704f1dcf299198d
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 15:57:49 -0300

    Demo of Custom exceptions: Solution 1 (very bad solution)- Validation logic in the main program

---
+++ Custom-exceptions/Custom-exceptions/Program.cs
@@ -0,0 +1,57 @@
+﻿using Custom_exceptions.Entities;
+using System;
+
+namespace Custom_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read the data of a hotel reservation (room number, check-in date and check-out date) and show the reservation data, 
+             *including its duration in days.
+             *Then, read new entry and exit dates, update the reservation, and show the reservation again with the updated data. The program must not accept
+             *invalid data for the reservation, according to the following rules:
+             *- Booking changes can only occur for future dates 
+             *- The exit date must be greater than the entry date*/
+
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+        }
+    }
+}
